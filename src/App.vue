872868fafe5d6ac3cbc1b478ab8ad492d4c3007c
Reece <trazhme@gmail.com>
2023-05-19 10:05:02 +0800

    update with emits

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,31 @@
         <!-- 
             props: src="https://im-awesome.me/your-awesome-image.jpg"
         -->
-        <input 
-            v-model="imgSrc" 
-            type="text" 
-            class="img-src"
-        />
+        {{ src }}
+        <hr />
+        {{ test }} 
+        <ImageSrc v-model:modelValue="src" v-model:testInput="test" @handleClick="handleClick"/>
+        
+        <input type="range" />
 
-        <MagnifierImage :src="imgSrc" />
+        <MagnifierImage :src="src" />
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import MagnifierImage from './components/MagnifierImage.vue';
+import ImageSrc from './components/ImageSrc.vue';
 
-const imgSrc = ref('https://images.pexels.com/photos/4175000/pexels-photo-4175000.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+const src = ref('https://images.pexels.com/photos/4175000/pexels-photo-4175000.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+
+const test = ref('');
+
+const handleClick = (e) => {
+    console.log('parent click', e)
+}
+
+
 </script>
 
 <style scoped>
---
+++ src/components/ImageSrc.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="image-src">
+        <input 
+            :value="modelValue" 
+            @input="$emit('update:modelValue', $event.target.value)"
+            type="text" 
+            class="img-src"
+        />
+
+        <input type="text" :value="testInput" @input="$emit('update:testInput', $event.target.value)" />
+
+        <button @click="handleClick()">click</button>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    modelValue: {
+        type: String,
+        default: 'https://images.pexels.com/photos/4175000/pexels-photo-4175000.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    testInput: {
+        type: String
+    }
+})
+
+const emit = defineEmits(['handleClick'])
+
+const data = [props.modelValue, props.testInput]
+const handleClick = () => {
+    console.log('child click', props)
+    emit('handleClick', props);
+}
+</script>
