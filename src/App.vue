f5d7b2c8c331660e101415f8c07c7a06ca6ca636
Reece <trazhme@gmail.com>
2023-05-19 10:10:21 +0800

    added ImgSrc component

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,7 @@
         <!-- 
             props: src="https://im-awesome.me/your-awesome-image.jpg"
         -->
-        <input 
-            v-model="imgSrc" 
-            type="text" 
-            class="img-src"
-        />
+        <ImgSrc v-model:imgSrc="imgSrc" />
 
         <MagnifierImage :src="imgSrc" />
     </div>
@@ -16,16 +12,8 @@
 <script setup>
 import { ref } from 'vue';
 import MagnifierImage from './components/MagnifierImage.vue';
+import ImgSrc from './components/ImgSrc.vue';
 
 const imgSrc = ref('https://images.pexels.com/photos/4175000/pexels-photo-4175000.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
 </script>
 
-<style scoped>
-.img-src {
-    width: 85%;
-    margin-bottom: 20px;
-    padding: 20px;
-    border: 1px solid #e6e6e6;
-    border-radius: 5px;
-}
-</style>
---
+++ src/components/ImgSrc.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="img-src">
+        <input 
+            :value="imgSrc" 
+            @input="$emit('update:imgSrc', $event.target.value)"
+            type="text" 
+            class="img-src-input"
+        />
+    </div>
+</template>
+
+<script setup>
+const { imgSrc } = defineProps({
+    imgSrc: {
+        type: String,
+        default: 'https://images.pexels.com/photos/4175000/pexels-photo-4175000.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    }
+})
+</script>
+
+<style scoped>
+.img-src-input {
+    width: 85%;
+    margin-bottom: 20px;
+    padding: 20px;
+    border: 1px solid #e6e6e6;
+    border-radius: 5px;
+}
+</style>
